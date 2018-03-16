ad3b9b167e2ec5efa4e788f6b4295e344c4edff8
mauroere <mauroere@gmail.com>
2018-03-16 14:24:30 -0300

    Update DataXplore_Analysis1.py

--- DataXplore_Analysis1.py
+++ DataXplore_Analysis1.py
@@ -101,3 +101,12 @@ plt.show()
 df_subset.plot(kind='scatter', x='duration', y='confidence', rot=70)
 plt.show()
 
+# Import necessary modules
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Create the boxplot
+df.boxplot(column='initial_cost', by='Borough', rot=90)
+
+# Display the plot
+plt.show()
